907ff880d90d2fbe68e7eb26b461a28e481a3dd9
moldovexc <alexanderparpulansky@gmail.com>
2023-07-17 15:57:00 +0300

    Refactored Error Messages

---
+++ src/main/java/com/example/productstoreapp/utils/ErrorMessages.java
@@ -0,0 +1,13 @@
+package com.example.productstoreapp.utils;
+
+public class ErrorMessages {
+    public static final String EMAIL_SEND_FAILURE = "Failed to send email";
+    public static final String EXPIRED_TOKEN = "Token expired";
+    public static final String EMAIL_ALREADY_CONFIRMED = "Email already confirmed";
+    public static final String TOKEN_NOT_FOUND = "Token not found";
+    public static final String USERNAME_ALREADY_EXISTS = "Username already exists!";
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists!";
+    public static final String NON_EXISTENT_USER = "User Does not exist";
+    public static final String NOT_CONFIRMED_EMAIL ="Email has not been confirmed!";
+    public static final String NEW_PASSWORD_NO_MATCH = "Please ensure the new password is entered correctly twice.";
+}
